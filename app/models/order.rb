205ac9985e7e7b29c2d3cabc313cbb0867c05709
Kabir Mohammed <kabirasm@gmail.com>
2011-01-10 18:48:14 +0000

    Rectified order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,8 +3,6 @@ class Order < ActiveRecord::Base
 
   validates_numericality_of :customer
   validates_numericality_of :company
-  validates :phone,  :presence   => true
-  validates :itemordered,  :presence   => true,
+  validates :itemordered,  :presence   => true
 		      
-
 end
